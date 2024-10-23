745b3e15fd9a608b2c12db413bb3bd7f3f4e6a81
Thiago <thiagopazjunior@gmail.com>
2024-10-22 23:35:31 -0300

    feat: initial config

---
+++ src/serve.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+app.get('/api/health-check', (req, res) => {
+  res.status(200).send("ok")
+});
+
+app.listen(3000, () => console.log("Serve is running"))
