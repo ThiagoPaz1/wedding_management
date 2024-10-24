b1539d48a5e3e39c38cb9e12b41b62a574d716fa
Thiago <thiagopazjunior@gmail.com>
2024-10-24 00:17:59 -0300

    f

--- src/serve.ts
+++ src/serve.ts
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 
 app.get('/api/health-check', (req, res) => {
-  res.status(200).send("ok!")
+  res.status(200).send("ok")
 });
 
 app.listen(3000, () => console.log("Serve is running"))
